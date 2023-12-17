d9b7a03c72007d7c19b97e61aa7e1dd1657eefd2
alonbeny <alonby@gmail.com>
2023-12-17 18:33:49 +0200

    read rules from rules.txt

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
-vectorstore = DocArrayInMemorySearch.from_texts(
-    ["MTG is a collectable card game",
-     "Each player starts the game with 20 life",
-     "Get your opponent's 20 life points down to 0 and you win the game",
-     "In MTG, a spell is any type of card cast by a player",
-     "Spells are usually cast from your hand",
-     "In special cases can be cast from other areas of the battlefield like your library or your graveyard",
-     "Land cards are the only type of card that is not considered a spell",
-     "Creature cards in Magic serve as your primary means to attack, defend, and activate abilities during your turn",
-     "They come in a wide array of shapes, sizes, and varying levels of power, so the damage dealt and received by your creatures will depend on these factors",
-     "Creatures can't attack the same turn they enter the battlefield; this is known as 'summoning sickness'"],
-    embedding=OpenAIEmbeddings(),
-)
+documents = TextLoader("data/rules.txt", encoding="utf8").load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+embedding = OpenAIEmbeddings()
+
+vectorstore = DocArrayInMemorySearch.from_documents(docs, embedding)
 retriever = vectorstore.as_retriever()
 
 template = """Answer the question based only on the following context:
